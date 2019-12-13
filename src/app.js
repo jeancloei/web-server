7bd7c4150f2e466fd2947a254403bf8657487d46
Jean Carlos De Castro <jeancarlosdecastro@Jeans-MacBook-Pro.local>
2019-12-13 13:15:41 -0500

    refactor using hbs for non-static page

--- src/app.js
+++ src/app.js
@@ -4,7 +4,28 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public') 
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Created by Jean'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Jean De Castro'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    message: 'Tell me what we can do for you today.'
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({
