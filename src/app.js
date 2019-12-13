506a77ffc3c268150579eebaa23eb01520a4bf77
Jean Carlos De Castro <jeancarlosdecastro@Jeans-MacBook-Pro.local>
2019-12-13 11:41:24 -0500

    added static path

--- src/app.js
+++ src/app.js
@@ -1,21 +1,32 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public') 
+
+app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
-  res.send('Hellow express!')
+  res.send('<h1>Hellow express!</hi>')
 })
 
 app.get('/help', (req, res) => {
-  res.send('Help page')
+  res.send([{
+    name: 'Jean'
+  }, {
+    age:36
+  }])
 })
 
 app.get('/about', (req, res) => [
-  res.send('About page')
+  res.send('<h1>About page</h1>')
 ])
 
 app.get('/weather', (req, res) => {
-  res.send('Weather page')
+  res.send({
+    forcast:"It is claudy",
+    location:'Miami, FL'
+  })
 })
 
 app.listen(3000, () => {
